3802b8f01cc2342d666e5d9244a2acad032f96aa
iroha <iroha@RUI.>
2026-04-04 17:54:44 +0800

    created a user model in models.py

--- server/models.py
+++ server/models.py
@@ -10,3 +10,18 @@ try:
     os.makedirs(app.instance_path)
 except OSError:
     pass
+
+
+class User(db.Model):
+    username = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    # password = db.Column(db.String(120), nullable=False)
+    # temporarily unsure about password storage, will implement when lectures mention plugins
+
+    volume_time = db.Column(db.Float, nullable=True, default=None)
+    brightness_time = db.Column(db.Float, nullable=True, default=None)
+    maze_time = db.Column(db.Float, nullable=True, default=None)
+    tnc_quiz_time = db.Column(db.Float, nullable=True, default=None)
+
+    # popups_time = db.Column(db.Float, nullable=True, default=None)
+    # not sure how we're going to implement popups, so leaving it out for now
